585de8a13b633c94787ffdabe773126afbdadf83
DominicHuynh <phucvbin2005@gmail.com>
2024-04-03 16:35:04 +1030

    w6 game

---
+++ warrior.h
@@ -0,0 +1,17 @@
+
+#ifndef WARRIOR_H
+#define WARRIOR_H
+#include "player.h"
+
+class Warrior : public Player
+{
+private:
+    string _weapon;
+
+public:
+    Warrior(string name, int health, int damage, string weapon);
+
+    void swingWeapon(Player *opponent);
+};
+
+#endif
